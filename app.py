f44abf15d077ae40a4f6549254c6c080b98d8a1b
adam <solarecho3@pm.me>
2024-02-03 15:27:37 -0500

    MVP data uploader to hex tree

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
+import uuid
+import json
+import pathlib
 import tomllib
+
 from typing import Any
 
+import pandas as pd
 import streamlit as st
 
 
@@ -16,6 +21,7 @@ config-version = "0.1"
 page_title = "HomeDocs"
 page_icon = ":shark:"
 layout = "centered"
+data_path_root = "data"
 """
     if not os.path.exists("config.toml"):
         with open("config.toml", "w") as f:
@@ -27,14 +33,101 @@ layout = "centered"
     return data
 
 
+def generate_hextree(root: str):
+    """Generate a hex tree folder structure.
+    :param root:
+    """
+    # os.makedirs(pathlib.Path(root), exist_ok=True)
+    for i in [hex(x)[2:] for x in range(0, 16)]:
+        for j in [hex(x)[2:] for x in range(0, 16)]:
+            for k in [hex(x)[2:] for x in range(0, 16)]:
+                os.makedirs(pathlib.PurePath(root, str(i), str(j), str(k)), exist_ok=True)
+
+
+def find_all_files(root):
+    import glob
+    files = glob.glob('root/**/*.jpg', recursive=True)
+    return files
+
+
+def query_hextree(filename: str):
+    """Query the data hex tree directory."""
+    pass
+
+
+def map_hextree(root: str, map_path: str):
+    """Map the data hex tree."""
+
+    # TODO: Save a default map JSON if none exists
+    # TODO: The root is the data path root
+    # TODO: The map path is from the configs; path to JSON
+
+    if not os.path.exists(os.path.join(root, map_path)):
+        pass
+
+
+def jpg_to_hextree(root, data, metadata):
+    """Save an image to hex tree directory."""
+    new_uid = uuid.uuid4()
+    _file = str(new_uid) + ".jpg"
+    metadata["uuid"] = str(new_uid)
+    metadata["extension"] = ".jpg"
+    _first = _file[0]
+    _second = _file[1]
+    _third = _file[2]
+    file_path = pathlib.Path(root, _first, _second, _third, _file)
+    metadata["full_path"] = str(file_path)
+    from datetime import datetime
+    metadata["upload_time"] = datetime.isoformat(datetime.utcnow())
+    metadata["upload_time_zone"] = "utc"
+    with open(file_path, "wb") as img:
+        img.write(data)
+
+    # create new empty
+    map_root = str(pathlib.Path(root, 'map.json'))
+    if not os.path.exists(map_root):
+        with open(map_root, "w") as f:
+            json.dump({}, f)
+
+    # read
+    with open(map_root, "r") as map_file:
+        map_data = json.load(map_file)
+
+    map_data[len(map_data.keys())] = metadata
+
+    with open(map_root, "w") as map_file:
+        json.dump(map_data, map_file)
+
+    st.success(f'Wrote image to hex: {file_path}')
+
+
 config = configs()
+DATA_PATH_ROOT = config['app']['data_path_root']
+
 st.set_page_config(
     page_title=config['app']['page_title'],
     page_icon=config['app']['page_icon'],
     layout=config['app']['layout'],
 )
 
+if not os.path.exists(DATA_PATH_ROOT):
+    generate_hextree(DATA_PATH_ROOT)
+
 st.header('Docs uploader')
 st.subheader('Upload')
-st.file_uploader('Upload doc.')
+st_file_uploader = st.file_uploader('New image', type='jpg', accept_multiple_files=False)
+st_file_uploader_description = st.text_input('Tags', placeholder='\"2023 1099 tax form\"...')
+st_file_uploader_submit = st.button('Upload')
 st.divider()
+
+if st_file_uploader_submit:
+    bytes_data = st_file_uploader.getvalue()
+
+    attach_metadata = dict(
+        original_filename=st_file_uploader.name,
+        tags=st_file_uploader_description,
+    )
+    jpg_to_hextree(DATA_PATH_ROOT, bytes_data, attach_metadata)
+
+st.subheader('Files in tree')
+st.dataframe(pd.read_json(DATA_PATH_ROOT + '/map.json').T)
